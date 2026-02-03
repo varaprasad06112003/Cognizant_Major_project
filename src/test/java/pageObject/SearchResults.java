f419eda1ef51acd3143622430f757d3dd6ac5279
varaprasad06112003 <116376144+varaprasad06112003@users.noreply.github.com>
2026-02-03 10:36:48 +0530

    Refactor clickLanguageFilter method for clarity

--- src/test/java/pageObject/SearchResults.java
+++ src/test/java/pageObject/SearchResults.java
@@ -44,12 +44,9 @@ public class SearchResults extends BasePage {
 
 
     public void clickLanguageFilter(){
-        //new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOf(languageFilter));
-        //languageFilter.click();
-        new WebDriverWait(driver, Duration.ofSeconds(15))
-    .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(text(),'Language')]")));
-WebElement languageFilter = driver.findElement(By.xpath("//div[contains(text(),'Language')]"));
-languageFilter.click();
+        new WebDriverWait(driver, Duration.ofSeconds(15)).until(ExpectedConditions.visibilityOf(languageFilter));
+        languageFilter.click();
+        
     }
 
     public void clickEnglishCheckbox(){
